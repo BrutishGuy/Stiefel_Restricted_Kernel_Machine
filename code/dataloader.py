b5c61a39b863193fa4e547d31cf9cadb977c65ef
Victor Gueorguiev <victor@amplifyanalytix.com>
2021-08-07 18:23:32 +0300

    Added transfer learning modification for performance improvements to Stiefel model with transfer learning.

--- code/dataloader.py
+++ code/dataloader.py
@@ -128,7 +128,7 @@ def get_imagenette_dataloader(args, path_to_data='imagenette2'):
         print('Data at the given path doesn\'t exist. Downloading now...')
         os.system(" mkdir imagenette2/;"
                   " wget -O imagenette2/imagenette2.tgz https://s3.amazonaws.com/fast-ai-imageclas/imagenette2.tgz ;"
-                  " cd imagenette2/; tar xzf imagenette2.tgz")
+                  " cd imagenette2/; tar xzf imagenette2.tar.gz")
 
     all_transforms = transforms.Compose([
         transforms.Resize(256),
