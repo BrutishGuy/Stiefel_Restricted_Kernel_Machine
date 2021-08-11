0109947065a9c7d24edf98389899a456a51f1f2d
Victor Gueorguiev <victor@amplifyanalytix.com>
2021-08-12 00:57:08 +0300

    Added transfer learning modification for performance improvements to Stiefel model with transfer learning.

--- code/stiefel_rkm_model_cifar.py
+++ code/stiefel_rkm_model_cifar.py
@@ -6,17 +6,32 @@ import stiefel_optimizer
 from dataloader import *
 from utils import Lin_View
 
-
 class Net1(nn.Module):
     """ Encoder - network architecture """
     def __init__(self, nChannels, args, cnn_kwargs):
         super(Net1, self).__init__()  # inheritance used here.
         self.args = args
+        self.main = nn.Sequential(
+            nn.Conv2d(nChannels, self.args.capacity, **cnn_kwargs[0]),
+            nn.GELU(),
+            
+            nn.Conv2d(self.args.capacity, self.args.capacity, kernel_size=3, padding=1),
+            nn.GELU(),
 
-        self.main = nn.Sequential(nn.Linear(self.args.num_ftrs, self.args.x_fdim1),
-                                  nn.GELU(),
-                                  nn.Linear(self.args.x_fdim1, self.args.x_fdim2)
-                                  )
+            nn.Conv2d(self.args.capacity, self.args.capacity * 2, **cnn_kwargs[0]),
+            nn.GELU(),
+
+            nn.Conv2d(self.args.capacity * 2, self.args.capacity * 2, kernel_size=3, padding=1),
+            nn.GELU(),
+            
+            nn.Conv2d(self.args.capacity * 2, self.args.capacity * 4, **cnn_kwargs[1]),
+            nn.GELU(),
+
+            nn.Flatten(),
+            nn.Linear(self.args.capacity * 4 * cnn_kwargs[2] ** 2, self.args.x_fdim1),
+            nn.GELU(),
+            nn.Linear(self.args.x_fdim1, self.args.x_fdim2),
+        )
 
     def forward(self, x):
         return self.main(x)
@@ -30,31 +45,30 @@ class Net3(nn.Module):
         self.main = nn.Sequential(
             nn.Linear(self.args.x_fdim2, self.args.x_fdim1),
             nn.GELU(),
-            nn.BatchNorm1d(self.args.x_fdim1),
             nn.Linear(self.args.x_fdim1, self.args.capacity * 4 * cnn_kwargs[2] ** 2),
             nn.GELU(),
-            nn.BatchNorm1d(self.args.capacity * 4 * cnn_kwargs[2] ** 2),
             Lin_View(self.args.capacity * 4, cnn_kwargs[2], cnn_kwargs[2]),  # Unflatten
 
-            nn.ConvTranspose2d(self.args.capacity * 4, self.args.capacity * 2, **cnn_kwargs[1]),
+            nn.ConvTranspose2d(self.args.capacity * 4, self.args.capacity * 2, output_padding=1, **cnn_kwargs[1]),
             nn.GELU(),
-            nn.BatchNorm2d(self.args.capacity * 2),
-            nn.ConvTranspose2d(self.args.capacity * 2, self.args.capacity, **cnn_kwargs[0]),
+            nn.Conv2d(self.args.capacity * 2, self.args.capacity * 2, kernel_size=3, padding=1),
             nn.GELU(),
-            nn.BatchNorm2d(self.args.capacity),
-
-            nn.ConvTranspose2d(self.args.capacity, nChannels, **cnn_kwargs[0]),
-            nn.Sigmoid()
+            nn.ConvTranspose2d(self.args.capacity * 2, self.args.capacity, output_padding=1, **cnn_kwargs[0]),
+            nn.GELU(),
+            nn.Conv2d(self.args.capacity, self.args.capacity, kernel_size=3, padding=1),
+            nn.GELU(),
+            nn.ConvTranspose2d(self.args.capacity, nChannels, output_padding=1, **cnn_kwargs[0]),
+            nn.Tanh()
         )
 
     def forward(self, x):
         return self.main(x)
 
 
-class RKM_Stiefel_Transfer(nn.Module):
+class RKM_Stiefel(nn.Module):
     """ Defines the Stiefel RKM model and its loss functions """
     def __init__(self, ipVec_dim, args, nChannels=1, recon_loss=nn.MSELoss(reduction='sum'), ngpus=1):
-        super(RKM_Stiefel_Transfer, self).__init__()
+        super(RKM_Stiefel, self).__init__()
         self.ipVec_dim = ipVec_dim
         self.ngpus = ngpus
         self.args = args
@@ -65,7 +79,7 @@ class RKM_Stiefel_Transfer(nn.Module):
         self.manifold_param = nn.Parameter(nn.init.orthogonal_(torch.Tensor(self.args.h_dim, self.args.x_fdim2)))
 
         # Settings for Conv layers
-        self.cnn_kwargs = dict(kernel_size=4, stride=2, padding=1)
+        self.cnn_kwargs = dict(kernel_size=3, stride=2, padding=1)
         if self.ipVec_dim <= 28*28*3:
             self.cnn_kwargs = self.cnn_kwargs, dict(kernel_size=3, stride=1), 5
         else:
@@ -73,13 +87,15 @@ class RKM_Stiefel_Transfer(nn.Module):
                 self.cnn_kwargs = self.cnn_kwargs, self.cnn_kwargs, 4 # change this bad boy to shift the dataset size, 28 for 224x224, 8 for 64x64 and 4 for 32x32
             elif self.args.dataset_name == 'cifar10' or self.args.dataset_name == 'imagenette':
                 self.cnn_kwargs = self.cnn_kwargs, self.cnn_kwargs, 8 # change this bad boy to shift the dataset size, 28 for 224x224, 8 for 64x64 and 4 for 32x32
+            elif self.args.dataset_name == 'galaxyzoo':
+                self.cnn_kwargs = self.cnn_kwargs, self.cnn_kwargs, 8 # change this bad boy to shift the dataset size, 28 for 224x224, 8 for 64x64 and 4 for 32x32
             else:
                 self.cnn_kwargs = self.cnn_kwargs, self.cnn_kwargs, 28 # change this bad boy to shift the dataset size, 28 for 224x224, 8 for 64x64 and 4 for 32x32
 
         self.encoder = Net1(self.nChannels, self.args, self.cnn_kwargs)
         self.decoder = Net3(self.nChannels, self.args, self.cnn_kwargs)
 
-    def forward(self, x, x_image_view):
+    def forward(self, x):
         op1 = self.encoder(x)  # features
         op1 = op1 - torch.mean(op1, dim=0)  # feature centering
         C = torch.mm(op1.t(), op1)  # Covariance matrix
@@ -87,27 +103,26 @@ class RKM_Stiefel_Transfer(nn.Module):
         """ Various types of losses as described in paper """
         if self.args.loss == 'splitloss':
             x_tilde1 = self.decoder(torch.mm(torch.mm(op1, self.manifold_param.t())
-                                            + self.args.noise_level * torch.randn((x_image_view.shape[0], self.args.h_dim)).to(self.args.proc),
+                                            + self.args.noise_level * torch.randn((x.shape[0], self.args.h_dim)).to(self.args.proc),
                                             self.manifold_param))
             x_tilde2 = self.decoder(torch.mm(torch.mm(op1, self.manifold_param.t()), self.manifold_param))
-            temp_view2 = x_image_view.view(-1, self.ipVec_dim)
-            temp_view1 = x_tilde1.view(-1, self.ipVec_dim)
-            temp_loss1 = self.recon_loss(temp_view1,  temp_view2)
-            temp_loss2 = self.recon_loss(x_tilde2.view(-1, self.ipVec_dim), x_tilde1.view(-1, self.ipVec_dim))
-            f2 = self.args.c_accu * 0.5 * (temp_loss1 + temp_loss2) / x_image_view.size(0)  # Recons_loss
+            f2 = self.args.c_accu * 0.5 * (
+                    self.recon_loss(x_tilde2.view(-1, self.ipVec_dim), x.view(-1, self.ipVec_dim))
+                    + self.recon_loss(x_tilde2.view(-1, self.ipVec_dim),
+                                      x_tilde1.view(-1, self.ipVec_dim))) / x.size(0)  # Recons_loss
 
         elif self.args.loss == 'noisyU':
             x_tilde = self.decoder(torch.mm(torch.mm(op1, self.manifold_param.t())
-                                            + self.args.noise_level * torch.randn((x_image_view.shape[0], self.args.h_dim)).to(self.args.proc),
+                                            + self.args.noise_level * torch.randn((x.shape[0], self.args.h_dim)).to(self.args.proc),
                                             self.manifold_param))
             f2 = self.args.c_accu * 0.5 * (
-                self.recon_loss(x_tilde.view(-1, self.ipVec_dim), x_image_view.view(-1, self.ipVec_dim))) / x_image_view.size(0)  # Recons_loss
+                self.recon_loss(x_tilde.view(-1, self.ipVec_dim), x.view(-1, self.ipVec_dim))) / x.size(0)  # Recons_loss
 
         elif self.args.loss == 'deterministic':
             x_tilde = self.decoder(torch.mm(op1, torch.mm(self.manifold_param.t(), self.manifold_param)))
-            f2 = self.args.c_accu * 0.5 * (self.recon_loss(x_tilde.view(-1, self.ipVec_dim), x_image_view.view(-1, self.ipVec_dim)))/x.size(0)  # Recons_loss
+            f2 = self.args.c_accu * 0.5 * (self.recon_loss(x_tilde.view(-1, self.ipVec_dim), x.view(-1, self.ipVec_dim)))/x.size(0)  # Recons_loss
 
-        f1 = torch.trace(C - torch.mm(torch.mm(self.manifold_param.t(), self.manifold_param), C))/x_image_view.size(0)  # KPCA
+        f1 = torch.trace(C - torch.mm(torch.mm(self.manifold_param.t(), self.manifold_param), C))/x.size(0)  # KPCA
         return f1 + f2, f1, f2
 
 # Accumulate trainable parameters in 2 groups:
@@ -125,26 +140,6 @@ def stiefel_opti(stief_param, lrg=1e-4):
     dict_g = {'params': stief_param, 'lr': lrg, 'momentum': 0.9, 'weight_decay': 0.0005, 'stiefel': True}
     return stiefel_optimizer.AdamG([dict_g])  # CayleyAdam
 
-def calculate_conv_output(input_size, cnn_kwargs):
-    assert type(cnn_kwargs) == dict
-    for cnn_kwarg_set in cnn_kwargs:
-        return
-
-
-def fetch_pretrained_model(model_name):
-    if model_name == 'resnet152':
-        # Freeze all parameters manually
-        model = models.resnet152(pretrained=True)
-        for param in model.parameters():
-            param.requires_grad = False
-        return model
-    if model_name == 'resnet50':
-        # Freeze all parameters manually
-        model = models.resnet50(pretrained=True)
-        for param in model.parameters():
-            param.requires_grad = False
-        return model
-
 def final_compute(model, args, ct, device=torch.device('cuda')):
     """ Utility to re-compute U. Since some datasets could exceed the GPU memory limits, some intermediate
     variables are saved  on HDD, and retrieved later"""
@@ -153,16 +148,10 @@ def final_compute(model, args, ct, device=torch.device('cuda')):
 
     args.shuffle = False
     x, _, _ = get_dataloader(args)
-    x_transfer_features = get_transfer_features(args)
-    
+
     # Compute feature-vectors
     for i, sample_batch in enumerate(tqdm(x)):
-        indices = sample_batch[2]
-
-        x_transfer_feature_batch = x_transfer_features.iloc[indices, :]
-        x_transfer_feature_batch = torch.tensor(x_transfer_feature_batch.drop(['Image'], axis = 1).values.astype(np.float32))
-                
-        torch.save({'oti': model.encoder(x_transfer_feature_batch.to(device))},
+        torch.save({'oti': model.encoder(sample_batch[0].to(device))},
                    'oti/oti{}_checkpoint.pth_{}.tar'.format(i, ct))
 
     # Load feature-vectors
---
+++ code/train_rkm_cifar.py
@@ -0,0 +1,172 @@
+from utils import create_dirs, convert_to_imshow_format
+from stiefel_rkm_model_cifar import *
+import logging
+import argparse
+import time
+import matplotlib.pyplot as plt
+from datetime import datetime
+import glob
+
+# Model Settings =================================================================================================
+parser = argparse.ArgumentParser(description='St-RKM Model',
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+parser.add_argument('--dataset_name', type=str, default='mnist',
+                    help='Dataset name: mnist/fashion-mnist/svhn/dsprites/3dshapes/cars3d')
+parser.add_argument('--h_dim', type=int, default=10, help='Dim of latent vector')
+parser.add_argument('--capacity', type=int, default=64, help='Conv_filters of network')
+parser.add_argument('--mb_size', type=int, default=256, help='Mini-batch size')
+parser.add_argument('--x_fdim1', type=int, default=256, help='Input x_fdim1')
+parser.add_argument('--x_fdim2', type=int, default=50, help='Input x_fdim2')
+parser.add_argument('--c_accu', type=float, default=1, help='Input weight on recons_error')
+parser.add_argument('--noise_level', type=float, default=1e-3, help='Noise-level')
+parser.add_argument('--loss', type=str, default='deterministic', help='loss type: deterministic/noisyU/splitloss')
+parser.add_argument('--transfer_learning', dest='use_transfer_learning', action='store_true', help='Use pre-training')
+parser.add_argument('--no_transfer_learning', dest='use_transfer_learning', action='store_false', help='Do not use pre-training')
+
+parser.add_argument('--transfer_learning_arch', type=str, default='resnet50', help='pre-trained architecture to use: one of those available in torchvision.models, e.g.) resnet50, resnet152, inception_v3, vgg19, etc.')
+parser.add_argument('--use_checkpoint', dest='resume_from_checkpoint', action='store_true', help='Start from checkpoint in case of crash or other early termination, i.e. continue training.')
+parser.add_argument('--no_use_checkpoint', dest='resume_from_checkpoint', action='store_false', help='Do not start from checkpoint, i.e. start from scratch.')
+parser.add_argument('--checkpoint_path', type=str, default='mnist', help='Sub-folder within in ./cp/ directory which contains the checkpoints from the runs.')
+parser.set_defaults(use_transfer_learning=False)
+parser.set_defaults(resume_from_checkpoint=False)
+# Training Settings =============================
+parser.add_argument('--lr', type=float, default=2e-4, help='Input learning rate for ADAM optimizer')
+parser.add_argument('--lrg', type=float, default=1e-4, help='Input learning rate for Cayley_ADAM optimizer')
+parser.add_argument('--max_epochs', type=int, default=1000, help='Input max_epoch')
+parser.add_argument('--proc', type=str, default='cuda', help='device type: cuda or cpu')
+parser.add_argument('--workers', type=int, default=4, help='Number of workers for dataloader')
+parser.add_argument('--shuffle', type=bool, default=True, help='shuffle dataset: True/False')
+
+opt = parser.parse_args()
+# ==================================================================================================================
+#opt.start_from_checkpoint = False
+
+device = torch.device(opt.proc)
+if torch.cuda.is_available():
+    torch.cuda.empty_cache()
+
+ct = time.strftime("%Y%m%d-%H%M")
+dirs = create_dirs(name=opt.dataset_name, ct=ct)
+dirs.create()
+
+# noinspection PyArgumentList
+logging.basicConfig(level=logging.INFO,
+                    format='%(message)s',
+                    handlers=[logging.FileHandler('log/{}/{}_{}.log'.format(opt.dataset_name, opt.dataset_name, ct)),
+                              logging.StreamHandler()])
+
+""" Load Training Data """
+xtrain, ipVec_dim, nChannels = get_dataloader(args=opt)
+
+""" Visualize some training data """
+# perm1 = torch.randperm(len(xtrain.dataset))
+# it = 0
+# fig, ax = plt.subplots(5, 5)
+# for i in range(5):
+#     for j in range(5):
+#         ax[i, j].imshow(convert_to_imshow_format(xtrain.dataset[perm1[it]][0].numpy()))
+#         it+=1
+# plt.suptitle('Ground Truth Data')
+# plt.setp(ax, xticks=[], yticks=[])
+# plt.show()
+
+ngpus = torch.cuda.device_count()
+
+#ngpus = 0 # uncomment this value and comment the above to force cpu usage in case of unsupported CUDA version on your GPU - this is sadly the simplest solution
+if opt.resume_from_checkpoint:
+  list_of_files = glob.glob('./cp/' + opt.checkpoint_path + '/*') # * means all if need specific format then *.csv
+  print('./cp/' + opt.dataset_name + '/*')
+  latest_file = max(list_of_files, key=os.path.getctime)
+  print('loading from last checkpoint: ' + str(latest_file))
+
+
+  sd_mdl = torch.load(latest_file)
+  
+  rkm = RKM_Stiefel(ipVec_dim=ipVec_dim, args=opt, nChannels=nChannels, ngpus=ngpus).to(device)
+  rkm.load_state_dict(sd_mdl['rkm_state_dict'])
+  param_g, param_e1 = param_state(rkm)
+
+  t = sd_mdl['epochs']
+  optimizer1 = stiefel_opti(param_g, opt.lrg)
+  optimizer2 = torch.optim.Adam(param_e1, lr=opt.lr, weight_decay=0)
+
+  optimizer1.load_state_dict(sd_mdl['optimizer1'])
+  optimizer2.load_state_dict(sd_mdl['optimizer2'])
+  Loss_stk = sd_mdl['Loss_stk']
+  cost = Loss_stk[-1][0]
+  l_cost = Loss_stk[-1][0]
+else:
+  rkm = RKM_Stiefel(ipVec_dim=ipVec_dim, args=opt, nChannels=nChannels, ngpus=ngpus).to(device)
+
+  # Accumulate trainable parameters in 2 groups:
+  # 1. Manifold_params 2. Network params
+  param_g, param_e1 = param_state(rkm)
+
+  optimizer1 = stiefel_opti(param_g, opt.lrg)
+  optimizer2 = torch.optim.Adam(param_e1, lr=opt.lr, weight_decay=0)
+  Loss_stk = np.empty(shape=[0, 3])
+  cost, l_cost = np.inf, np.inf  # Initialize cost
+  t = 1
+
+logging.info(rkm)
+logging.info(opt)
+logging.info('\nN: {}, mb_size: {}'.format(len(xtrain.dataset), opt.mb_size))
+logging.info('We are using {} GPU(s)!'.format(ngpus))
+
+# Train =========================================================================================================
+start = datetime.now()
+is_best = False
+
+while cost > 1e-10 and t <= opt.max_epochs:  # run epochs until convergence or cut-off
+    avg_loss, avg_f1, avg_f2 = 0, 0, 0
+
+    for _, sample_batched in enumerate(tqdm(xtrain, desc="Epoch {}/{}".format(t, opt.max_epochs))):
+        loss, f1, f2 = rkm(sample_batched[0].to(device))
+
+        optimizer1.zero_grad()
+        optimizer2.zero_grad()
+        loss.backward()
+        optimizer2.step()
+        optimizer1.step()
+
+        avg_loss += loss.item()
+        avg_f1 += f1.item()
+        avg_f2 += f2.item()
+    cost = avg_loss
+
+    # Remember lowest cost and save checkpoint
+    is_best = cost < l_cost
+    l_cost = min(cost, l_cost)
+    dirs.save_checkpoint({
+        'epochs': t,
+        'rkm_state_dict': rkm.state_dict(),
+        'optimizer1': optimizer1.state_dict(),
+        'optimizer2': optimizer2.state_dict(),
+        'Loss_stk': Loss_stk,
+    }, is_best)
+
+    logging.info('Epoch {}/{}, Loss: [{}], Kpca: [{}], Recon: [{}]'.format(t, opt.max_epochs, cost, avg_f1, avg_f2))
+    Loss_stk = np.append(Loss_stk, [[cost, avg_f1, avg_f2]], axis=0)
+    t += 1
+
+logging.info('Finished Training. Lowest cost: {}'
+             '\nLoading best checkpoint [{}] & computing sub-space...'.format(l_cost, dirs.dircp))
+# ==================================================================================================================
+
+# Load Checkpoint
+sd_mdl = torch.load('cp/{}/{}'.format(opt.dataset_name, dirs.dircp))
+rkm.load_state_dict(sd_mdl['rkm_state_dict'])
+
+h, U = final_compute(model=rkm, args=opt, ct=ct)
+logging.info("\nTraining complete in: " + str(datetime.now() - start))
+
+# Save Model and Tensors ======================================================================================
+torch.save({'rkm': rkm,
+            'rkm_state_dict': rkm.state_dict(),
+            'optimizer1': optimizer1.state_dict(),
+            'optimizer2': optimizer2.state_dict(),
+            'Loss_stk': Loss_stk,
+            'opt': opt,
+            'h': h, 'U': U}, 'out/{}/{}'.format(opt.dataset_name, dirs.dirout))
+logging.info('\nSaved File: {}'.format(dirs.dirout))
